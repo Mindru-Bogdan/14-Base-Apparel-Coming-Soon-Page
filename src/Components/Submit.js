d75fa8df8ec86dce05528d81afd4b94b8eb0fbde
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-06-09 13:18:57 +0300

    Device Friendly Solution

--- src/Components/Submit.js
+++ src/Components/Submit.js
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+
 import Arrow from '../../src/icon-arrow.svg';
+import Error from '../../src/icon-error.svg';
 
 import classes from './Submit.module.css';
 
 const Submit = () => {
+  const [email, setEmail] = useState('');
+  const [validity, setValidity] = useState(true);
+
+  const emailSet = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const isEmailValid = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const ValidityHandler = (event) => {
+    if (!isEmailValid(email)) {
+      setValidity(false);
+    } else {
+      setValidity(true);
+    }
+  };
+
   return (
     <form>
-      <input type="text" placeholder="Email Address" />
-      <img className={classes.arrow} src={Arrow} />
+      <input type="text" placeholder="Email Address" onChange={emailSet} />
+      <img className={classes.arrow} src={Arrow} onClick={ValidityHandler} />
+      <img className={validity ? classes.hidden : classes.error} src={Error} />
+      <p className={validity ? classes.hidden : classes.errorText}>Please provide a valid email</p>
     </form>
   );
 };
